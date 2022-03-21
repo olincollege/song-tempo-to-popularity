d922e498d05eacd83852166db5fac00e72f2a491
richardli03 <rli@olin.edu>
2022-03-21 12:05:28 -0400

    created gitignore + started bpm script

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This program should be able to do the following:
 * Fetch the most popular songs from the Billboard Top 100
 * Find the tempo/energy of those songs using Spotify's API
 * Produce a visual indicating how tempo correlates with song popularity.
+
+
+# dependencies:
+[spotipy](https://spotipy.readthedocs.io/en/2.19.0/): a lightweight spotify python wrapper.
--- scripts/bpm.py
+++ scripts/bpm.py
@@ -5,3 +5,51 @@ Produces a csv matching individual songs to popularity and BPM
 
 NOTE: We don't care what the songs are, we just care about the popularity v.s. BPM
 """
+
+import requests
+import spotipy
+import json
+from spotipy.oauth2 import SpotifyClientCredentials
+import yaml
+
+with open ("../config.yaml","r") as yaml_file:
+    config = yaml.safe_load(yaml_file)
+
+
+
+#Authentication - without user
+
+
+# print(config["SPOTIPY_CLIENT_SECRET"])
+
+
+client_credentials_manager = SpotifyClientCredentials(client_id=config["SPOTIPY_CLIENT_ID"], client_secret=config["SPOTIPY_CLIENT_SECRET"])
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+
+for track in sp.playlist_tracks(playlist_URI)["items"]:
+    #URI
+    track_uri = track["track"]["uri"]
+    
+    #Track name
+    track_name = track["track"]["name"]
+    
+    #Main Artist
+    artist_uri = track["track"]["artists"][0]["uri"]
+    artist_info = sp.artist(artist_uri)
+    
+    #Name, popularity, genre
+    artist_name = track["track"]["artists"][0]["name"]
+    artist_pop = artist_info["popularity"]
+    artist_genres = artist_info["genres"]
+    
+    #Album
+    album = track["track"]["album"]["name"]
+    
+    #Popularity of the track
+    track_pop = track["track"]["popularity"]
+    
+    sp.audio_features(track_uri)[0]
