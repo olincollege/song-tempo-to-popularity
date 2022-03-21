451d6f32c5cc81671f016f66cbc449b469ea2772
richardli03 <rli@olin.edu>
2022-03-21 13:30:55 -0400

    can now produce audio features from track+artist name

--- scripts/bpm.py
+++ scripts/bpm.py
@@ -6,9 +6,8 @@ Produces a csv matching individual songs to popularity and BPM
 NOTE: We don't care what the songs are, we just care about the popularity v.s. BPM
 """
 
-import requests
-import spotipy
 import json
+import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import yaml
 
@@ -17,19 +16,45 @@ with open ("../config.yaml","r") as yaml_file:
 
 
 
-#Authentication - without user
+# Authentication - without user
 
 
 # print(config["SPOTIPY_CLIENT_SECRET"])
 
-
 client_credentials_manager = SpotifyClientCredentials(client_id=config["SPOTIPY_CLIENT_ID"], client_secret=config["SPOTIPY_CLIENT_SECRET"])
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
+
+# grabs a playlist
 playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
 playlist_URI = playlist_link.split("/")[-1].split("?")[0]
-track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+playlist_items = sp.playlist_tracks(playlist_URI)["items"]
+playlist = playlist_items[2]["track"]["name"]
 
+
+track_link = ""
+track_URI = track_link.split("/")[-1].split("?")[0]
+
+# print(playlist)
+# track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+
+track = "Favorite Boy"
+artist = "Aeio"
+searchQuery = track + ' ' + artist 
+searchResults = sp.search(q=searchQuery)
+
+print(type(searchResults))
+# print(type(searchResults))
+
+uri = searchResults["tracks"]["items"][0]["uri"]
+print(uri)
+# view the results of the search
+# print(json.dumps(searchResults,indent = 4))
+
+audio_features = sp.audio_features(uri)
+print(audio_features[0]["tempo"])
+
+"""
 for track in sp.playlist_tracks(playlist_URI)["items"]:
     #URI
     track_uri = track["track"]["uri"]
@@ -48,8 +73,10 @@ for track in sp.playlist_tracks(playlist_URI)["items"]:
     
     #Album
     album = track["track"]["album"]["name"]
+    # print(track_name)
     
     #Popularity of the track
     track_pop = track["track"]["popularity"]
     
     sp.audio_features(track_uri)[0]
+"""
