3492925695f851a1050e4c7661ba88026865589a
richardli03 <rli@olin.edu>
2022-03-29 17:04:28 -0400

    bpm data added

--- scripts/bpm.py
+++ scripts/bpm.py
@@ -1,69 +1,102 @@
 """ 
-Scraping Spotify for all of the songs found in billboard.py
+This file scrapes Spotify, querying it for all of the songs found in billboard.py
+and produces a csv of tempos. The datafile is just numbers because we don't care
+what the songs are, we just care about the tempo data.
 
-Produces a csv matching individual songs to popularity and BPM
 
-NOTE: We don't care what the songs are, we just care about the popularity v.s. BPM
+NOTE: the main for loop of this file isn't written in a function because that adds
+an extra level of complexity that isn't necessary. We won't ever be looping and adding
+elements to a dataframe except in the context of filling out tempo information in this
+file. That's why there's a docstring up here explaining what this entire file does.
 """
 
+from time import sleep
 import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import yaml
 from ast import literal_eval
 
+
+# import the YAML file as a configuration file so that we can request spotify privately
+# DON'T UPLOAD CONFIG.YAML TO GITHUB
 with open ("../config.yaml","r") as yaml_file:
     config = yaml.safe_load(yaml_file)
 
+
+# Define a function that will query spotify and return the tempo of a song
 def query_spotify(track,artist):
+    """
+    Queries Spotify's API for the tempo information corresponding a track by an artist.
+    
+    Args:
+        track (str): the track to find bpm info for
+        artist (str): the artist of that track
+
+    Returns:
+        audio_features["tempo] (int): an integer containing the BPM of that song
+        
+        OR 
+        
+        ValueError: if no song is found corresponding to the query
+    """
     searchQuery = track + ' ' + artist 
     searchResults = sp.search(q=searchQuery)
     uri = searchResults["tracks"]["items"][0]["uri"]
     audio_features = sp.audio_features(uri)[0]
     return audio_features["tempo"]
 
+# Define a range of years that you'd like to start and stop querying
+start_year = 2020
+end_year = 2021
 
-# Authentication - without user
-
-
-# print(config["SPOTIPY_CLIENT_SECRET"])
-
+# Open client to begin requesting data
 client_credentials_manager = SpotifyClientCredentials(client_id=config["SPOTIPY_CLIENT_ID"], client_secret=config["SPOTIPY_CLIENT_SECRET"])
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-track = "Favorite Boy"
-artist = "Aeio"
-
-
-# print(query_spotify(track,artist))
-
-# data = {''}
+# Open the datafile from billboard scraping
 song_df = pd.read_csv("../data/top100.csv").set_index("index")
 
-tempo_df = pd.DataFrame(index = range(1,101), columns = range(1940,2022))
-
-# next = literal_eval(df["2018"][0])
-
+# Initialize an empty dataframe where tempo information will be stored
+tempo_df = pd.DataFrame(index = range(1,101), columns = range(start_year,end_year))
 
 
-for i in range(1,101):
-    next_entry = literal_eval(song_df["2018"][i])
-    track = next_entry[1]
-    artist = next_entry[2]
-    try: 
-        tempo_df.at[i,2018] = query_spotify(track,artist)
-    except IndexError:
-        tempo_df.at[i,2018] = None
-print(tempo_df)
-tempo_df.to_csv("../data/bpm.csv")
+
+# Iterate through all of the years of data
+for i in range(start_year,end_year):
+    print("i is now: ", str(i))
+    
+    # Iterate through all 100 songs for each year
+    for j in range(1,101):
+        print("we are now on the", str(j),"th song")
+        
+        # Each cell is a list, but it's saved as a string right now
+        # literal_eval will evaluate it as a list
+        try:
+            next_entry = literal_eval(song_df[str(i)][j])
+            
+        # If there is no song in that cell go to next line
+        except ValueError:
+            break
+        
+        # The track + artist will be the 2nd and 3rd element of the list in the cell
+        track = next_entry[1]
+        artist = next_entry[2]
+        
+        # Try to query spotify for BPM info
+        try: 
+            tempo_df.at[j,i] = query_spotify(track,artist)
+            
+        # If song isn't found, set the value as NaN so it doesn't mess with the average
+        except Exception as e:
+            print(e)
+            
+            tempo_df.at[j,i] = None
+    
+    # Pause to avoid over-requesting Spotify
+    sleep(1)
+    
+# Write the resulting dataframe to a CSV file that visualization.py can use
+tempo_df.to_csv("../data/bpmFULL.csv")
 
 
-# df.at[1,1940] = query_spotify(track,artist)
-# print(df)
-
-# with open ("../data/bpm.csv","a") as f:
-#     dictwriter = csv.DictWriter(f, fieldnames = headers)
-#     new_entry = query_spotify(track,artist)
-#     print(type(new_entry["tempo"]))
-
-# print(audio_features[0]["energy"])
